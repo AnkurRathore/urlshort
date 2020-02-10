ec4a847ffa022211fa270edfc9fcd9bc1680d296
Ankur <ankur@intuitorit.com>
2020-02-10 17:26:07 +0530

    tested the yaml handler

--- main/main.go
+++ main/main.go
@@ -11,13 +11,23 @@ func main() {
 
 	mux := defaultMux()
 	pathsToUrls := map[string]string{
-		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/urlshort-godoc": "https://github.com/AnkurRathore/urlshort",
 		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
 	}
 	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 
+	yaml := `
+- path: /urlshort
+  url: https://github.com/AnkurRathore/urlshort
+- path: /godoc
+  url: https://godoc.org/
+`
+	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	if err != nil {
+		panic(err)
+	}
 	fmt.Println("Starting the server on :9000")
-	http.ListenAndServe(":9000", mapHandler)
+	http.ListenAndServe(":9000", yamlHandler)
 
 }
 
