a4bcaebb7701b2aefc48607cd2b5151753f320b8
Ankur <ankur@intuitorit.com>
2020-02-10 17:18:47 +0530

    added yaml handler

--- handler.go
+++ handler.go
@@ -1,6 +1,10 @@
 package urlshort
 
-import "net/http"
+import (
+	"net/http"
+
+	yaml "gopkg.in/yaml.v2"
+)
 
 // Map handler will return an http.Handlerfunc that
 // will attempt to map any paths to their corresponding
@@ -23,4 +27,40 @@ func MapHandler(pathToUrls map[string]string, fallback http.Handler) http.Handle
 		// else
 		fallback.ServeHTTP(w, r)
 	}
+}
+
+func YAMLHandler(yamlBytes []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	pathUrls, err := parseYaml(yamlBytes)
+
+	if err != nil {
+		return nil, err
+	}
+	pathToUrls := buildMap(pathUrls)
+
+	return MapHandler(pathToUrls, fallback), nil
+
+}
+
+func buildMap(pathUrls []pathUrl) map[string]string {
+	pathsToUrls := make(map[string]string)
+
+	for _, pu := range pathUrls {
+		pathsToUrls[pu.Path] = pu.URL
+	}
+
+	return pathsToUrls
+}
+
+func parseYaml(data []byte) ([]pathUrl, error) {
+	var pathUrls []pathUrl
+	err := yaml.Unmarshal(data, &pathUrls)
+	if err != nil {
+		return nil, err
+	}
+	return pathUrls, err
+}
+
+type pathUrl struct {
+	Path string `yaml::`
+	URL  string
 }
---
+++ main/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/AnkurRathore/urlshort"
+)
+
+func main() {
+
+	mux := defaultMux()
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
+
+	fmt.Println("Starting the server on :9000")
+	http.ListenAndServe(":9000", mapHandler)
+
+}
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello,world!")
+}
